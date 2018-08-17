3c80c90691f7d6cb8de19547d9bf4db2636f4cfe
Ali Mohamad <alimohamad1122@gmail.com>
2018-08-17 19:56:46 -0400

    wip

--- wip.py
+++ wip.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 
 import os
-import emoji
 
 print("WORK (adding)")
-print(emoji.emojize("""
+print("""
       \U+1F61C:
     \U+1F44A /||\_ 
      - -
-   _/   \_ """, use_aliases=True))
+   _/   \_ """)
 os.system("git add .")
 
 print("IN (committing)")
-print(emoji.emojize("""
+print("""
     \U+1F44B  
      \ \U+1F633
        || \_
       - -
-    _/   \_ """, use_aliases=True))
+    _/   \_ """)
 os.system('git commit  -m "wip" ')
 
 print("PROGRESS (pushing)")
-print(emoji.emojize("""
+print("""
             \U+1F44B       
         \U+1F629  /
      _/ ||
         - -
-      _/   \_""", use_aliases=True))
+      _/   \_""")
 os.system("git push origin master")
